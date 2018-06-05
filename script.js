af1387b91a2fb228c57a0019a2cc1ab0c238751f
Rolf Strathewerd <rolf@strathewerd.de>
2018-06-05 08:52:35 +0200

    FIX: getDay -> getDate

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ jQuery(document).ready(function() {
             var dt = new Date();
             id = ns.val()+":"+dt.getFullYear()  +
                 ("0" + (dt.getMonth()+1)).slice(-2) +
-                ("0" + dt.getDay()).slice(-2) + '-' +
+                ("0" + dt.getDate()).slice(-2) + '-' +
                 ("0" + dt.getHours()).slice(-2) +
                 ("0" + dt.getMinutes()).slice(-2) +
                 ("0" + dt.getSeconds()).slice(-2);
